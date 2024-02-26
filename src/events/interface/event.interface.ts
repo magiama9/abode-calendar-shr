e2b623de8f61b296a224e6ee7bcdffdbd3c9b85e
Sam Randels <samrandels@gmail.com>
2024-02-26 16:05:46 -0500

    Fix: Update event interface to reflect current data structure

--- src/events/interface/event.interface.ts
+++ src/events/interface/event.interface.ts
@@ -3,6 +3,8 @@ import { Document } from 'mongoose';
 export interface Event extends Document {
   title: string;
   description: string;
-  date: Date;
+  start: Date;
+  end: Date;
+  // date: Date;
   invitees: Array<string>;
 }
