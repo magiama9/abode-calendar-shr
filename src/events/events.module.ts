ac84ebb0f6445318abe423240dc1f72b7185e16a
Sam Randels <samrandels@gmail.com>
2024-02-27 18:18:49 -0500

    Add: Unused authentication functionality

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventSchema } from './schema/event.schema';
+import { AuthenticationMiddleware } from '../shared/authentication.middleware';
 
 @Module({
   imports: [
@@ -12,3 +18,16 @@ import { EventSchema } from './schema/event.schema';
   providers: [EventsService],
 })
 export class EventsModule {}
+
+// export class EventsModule implements NestModule {
+//   configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+//     consumer
+//       .apply(AuthenticationMiddleware)
+//       .forRoutes(
+//         { method: RequestMethod.GET, path: '/events' },
+//         { method: RequestMethod.POST, path: '/events/' },
+//         { method: RequestMethod.PATCH, path: '/events' },
+//         { method: RequestMethod.DELETE, path: '/events/' },
+//       );
+//   }
+// }
