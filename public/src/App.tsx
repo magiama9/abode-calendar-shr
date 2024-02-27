aa1b9b7a591634c8863fc95a8a30ba4baa1b1ac4
Sam Randels <samrandels@gmail.com>
2024-02-27 18:10:37 -0500

    Cleanup: Clean up old code and add import for landing page

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,17 +1,17 @@
 // import { useState } from 'react';
 import Cal from './components/calendar';
+import LandingPage from './components/landingPage';
 import './App.css';
+// import { useAuth0 } from './utils/contexts/auth0-context';
 
 function App() {
+  // const { isAuthenticated, getIdTokenClaims, user } = useAuth0();
+
   return (
     <>
+      {/* <LandingPage /> */}
       <div>
         <Cal />
-      </div>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
       </div>
     </>
   );
