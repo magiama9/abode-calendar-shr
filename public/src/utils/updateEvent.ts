4cb4378da3ef8c8a8fa4acd5f6fca7c9c7f23e0c
Sam Randels <samrandels@gmail.com>
2024-02-27 18:17:37 -0500

    Init: Event updating handler

---
+++ public/src/utils/updateEvent.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const updateEvent = async (event) => {
+    try {
+      const response = await axios.patch('http://127.0.0.1:5001/events/' + event.eventId, event);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
